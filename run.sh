3bce1ad9fb7994cf32a05f84db973ee0b4e0c471
jwgcurrie <joelcurrie3@gmail.com>
2025-10-12 23:53:24 +0100

    removed nvidia dependency for non nvidia hardware, added permissions for x11 forwarding

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+xhost +local:docker
 
 # Detect if the host has an NVIDIA GPU
 if [ -x "$(command -v nvidia-smi)" ]; then
@@ -8,7 +9,7 @@ if [ -x "$(command -v nvidia-smi)" ]; then
                  -e NVIDIA_VISIBLE_DEVICES=all"
 else
     echo "No NVIDIA GPU detected. Using default GPU settings."
-    DOCKER_ARGS="--gpus all"
+    DOCKER_ARGS=""
 fi
 
 docker run -it --rm \
@@ -17,4 +18,4 @@ docker run -it --rm \
     $DOCKER_ARGS \
     --name pepper-container \
     --entrypoint /bin/bash \
-    pepper-direct-env
+    jwgcurrie/pepper-direct-env
